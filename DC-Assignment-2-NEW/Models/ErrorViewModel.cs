1f123fea4f7cf08d34a8b986df8f39267ec08c21
tokjiayi <700034826@student.curtin.edu.my>
2023-10-10 13:46:58 +0800

    Add project files.

---
+++ DC-Assignment-2-NEW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DC_Assignment_2_NEW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
