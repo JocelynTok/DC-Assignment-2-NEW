2db794e9943e78f41b729d309be94760e7885c75
shuman08 <shuman.lim@student.curtin.edu.au>
2023-10-11 10:59:12 +0800

    Updated userprofile

---
+++ DC-Assignment-2-NEW/Models/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace DC_Assignment_2_NEW.Models
+{
+    public class UserProfile
+    {
+
+        public string Username { get; set; } // User's username
+        public string Email { get; set; } // User's email address
+        public string Address { get; set; } // User's home address
+        public string Phone { get; set; } // User's phone number
+        public string PictureUrl { get; set; } // URL or path to the user's profile picture
+        public string PasswordHash { get; set; } // Hashed password
+        public string AccountNo { get; set; } // User's bank account number
+        public string Roles { get; set; } // User's role (admin or employee)
+    }
+}
