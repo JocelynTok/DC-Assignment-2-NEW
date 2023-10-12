48abcff131d5eb1ac9950e6990cbabcfc258bece
shuman08 <shuman.lim@student.curtin.edu.au>
2023-10-12 23:18:55 +0800

    solving merge conflict

--- DC-Assignment-2-NEW/Data/DBManager.cs
+++ DC-Assignment-2-NEW/Data/DBManager.cs
@@ -6,7 +6,6 @@ namespace DC_Assignment_2_NEW.Data
     public class DBManager
     {
         private static string connectionString = "Data Source=mydatabase.db;Version=3;";
-        
 
         public static bool CreateTable()
         {
@@ -53,7 +52,7 @@ namespace DC_Assignment_2_NEW.Data
                 {
                     connection.Open();
                     //Create a new SQLite command to execute SQL
-                    using(SQLiteCommand command = connection.CreateCommand())
+                    using (SQLiteCommand command = connection.CreateCommand())
                     {
                         //SQL Command to create a table named "Student Table"
                         command.CommandText = @"
@@ -64,14 +63,14 @@ namespace DC_Assignment_2_NEW.Data
                             AccountNo TEXT
                         )";
 
-                       command.ExecuteNonQuery();
+                        command.ExecuteNonQuery();
                         connection.Close();
                     }
                     Console.WriteLine("Table created successfully.");
                     return true;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
@@ -159,14 +158,14 @@ namespace DC_Assignment_2_NEW.Data
             return false; // Insertion failed
         }
 
-        public static bool InsertTransaction(Transaction transaction) 
+        public static bool InsertTransaction(Transaction transaction)
         {
             try
             {
-                using(SQLiteConnection connection = new SQLiteConnection(connectionString))
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
                 {
                     connection.Open();
-                    using(SQLiteCommand command = connection.CreateCommand())
+                    using (SQLiteCommand command = connection.CreateCommand())
                     {
                         command.CommandText = @"
                         INSERT INTO TransactionTable (TransactionID, TransactionType, Amount, AccountNo)
@@ -183,15 +182,17 @@ namespace DC_Assignment_2_NEW.Data
 
                         //check if any rows were inserted
                         connection.Close();
-                        if(rowsInserted > 0)
+                        if (rowsInserted > 0)
                         {
+                            // Update the account's balance
+                            UpdateAccountBalance(transaction.AccountNo, transaction.TransactionType, transaction.Amount);
                             return true; //insertion was successful
                         }
                     }
                     connection.Close();
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("Error transaction: " + ex.Message);
             }
@@ -221,6 +222,7 @@ namespace DC_Assignment_2_NEW.Data
                         connection.Close();
                         if (rowsDeleted > 0)
                         {
+
                             return true; // Deletion was successful
                         }
                     }
@@ -236,6 +238,7 @@ namespace DC_Assignment_2_NEW.Data
             }
         }
 
+        /*
         public static bool DeleteTransaction(string transactionID)
         {
             try
@@ -263,6 +266,64 @@ namespace DC_Assignment_2_NEW.Data
                         }
                     }
                     connection.Close();
+                }
+
+                return false; // No rows were deleted
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error deleting transaction: " + ex.Message);
+                return false; // Deletion failed
+            }
+        }*/
+
+        public static bool DeleteTransaction(string transactionID)
+        {
+            try
+            {
+                // Retrieve the transaction details using the GetTransactionByID method
+                Transaction transaction = GetTransactionByID(transactionID);
+
+                if (transaction != null)
+                {
+                    string accountNo = transaction.AccountNo;
+                    int amount = transaction.Amount;
+                    string transactionType = transaction.TransactionType;
+
+                    // Delete the transaction
+                    using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        using (SQLiteCommand command = connection.CreateCommand())
+                        {
+                            // Build the SQL command to delete the transaction by ID
+                            command.CommandText = $"DELETE FROM TransactionTable WHERE TransactionID = @TransactionID";
+                            command.Parameters.AddWithValue("@TransactionID", transactionID);
+
+                            // Execute the SQL command to delete the transaction
+                            int rowsDeleted = command.ExecuteNonQuery();
+
+                            if (rowsDeleted > 0)
+                            {
+                                // Update the account balance based on the deleted transaction type
+                                if (transactionType == "DEPOSIT")
+                                {
+                                    // If it was a deposit, subtract the amount
+                                    UpdateAccountBalance(accountNo, "DEPOSIT", -amount);
+                                }
+                                else if (transactionType == "WITHDRAW")
+                                {
+                                    // If it was a withdrawal, add back the amount
+                                    UpdateAccountBalance(accountNo, "WITHDRAW", -amount);
+                                }
+
+                                return true; // Deletion was successful
+                            }
+                        }
+
+                        connection.Close();
+                    }
                 }
 
                 return false; // No rows were deleted
@@ -323,26 +384,54 @@ namespace DC_Assignment_2_NEW.Data
                 {
                     connection.Open();
 
-                    // Create a new SQLite command to execute SQL
-                    using (SQLiteCommand command = connection.CreateCommand())
-                    {
-                        // Build the SQL command to update data by ID
-                        command.CommandText = $"UPDATE TransactionTable SET AccountNo = @AccountNo, TransactionType = @TransactionType , Amount = @Amount WHERE TransactionID = @TransactionID";
-                        command.Parameters.AddWithValue("@TransactionID", transaction.TransactionID);
-                        command.Parameters.AddWithValue("@TransactionType", transaction.TransactionType);
-                        command.Parameters.AddWithValue("@Amount", transaction.Amount);
-                        command.Parameters.AddWithValue("@AccountNo", transaction.AccountNo);
+                    // Retrieve the old transaction information to calculate the change in amount and type
+                    Transaction oldTransaction = GetTransactionByID(transaction.TransactionID);
 
-                        // Execute the SQL command to update data
-                        int rowsUpdated = command.ExecuteNonQuery();
-                        connection.Close();
-                        // Check if any rows were updated
-                        if (rowsUpdated > 0)
+                    if (oldTransaction != null)
+                    {
+                        int oldAmount = oldTransaction.Amount;
+                        string oldTransactionType = oldTransaction.TransactionType;
+
+                        // Build the SQL command to update data by ID
+                        using (SQLiteCommand command = connection.CreateCommand())
                         {
-                            return true; // Update was successful
+                            command.CommandText = "UPDATE TransactionTable SET TransactionType = @TransactionType, Amount = @Amount WHERE TransactionID = @TransactionID";
+                            command.Parameters.AddWithValue("@TransactionID", transaction.TransactionID);
+                            command.Parameters.AddWithValue("@TransactionType", transaction.TransactionType);
+                            command.Parameters.AddWithValue("@Amount", transaction.Amount);
+
+                            // Execute the SQL command to update data
+                            int rowsUpdated = command.ExecuteNonQuery();
+                            connection.Close();
+
+                            // Check if any rows were updated
+                            if (rowsUpdated > 0)
+                            {
+                                // Calculate the change in amount
+                                int amountChange = transaction.Amount - oldAmount;
+
+                                // Calculate the change in the transaction type
+                                if (transaction.TransactionType != oldTransactionType)
+                                {
+                                    if (transaction.TransactionType == "DEPOSIT")
+                                    {
+                                        Console.WriteLine("Old Amount :" + oldAmount);
+                                        Console.WriteLine("New Amount :" + transaction.Amount);
+                                        amountChange = oldAmount + transaction.Amount;
+                                    }
+                                    else if (transaction.TransactionType == "WITHDRAW")
+                                    {
+                                        amountChange -= oldAmount;
+                                    }
+                                }
+
+                                // Update the account balance based on the amount change
+                                UpdateAccountBalance(transaction.AccountNo, transaction.TransactionType, amountChange);
+
+                                return true; // Update was successful
+                            }
                         }
                     }
-                    connection.Close();
                 }
 
                 return false; // No rows were updated
@@ -351,6 +440,77 @@ namespace DC_Assignment_2_NEW.Data
             {
                 Console.WriteLine("Error updating transaction: " + ex.Message);
                 return false; // Update failed
+            }
+        }
+
+        public static int GetTransactionAmount(string transactionID)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand command = connection.CreateCommand())
+                    {
+                        // Build the SQL command to select the transaction by ID
+                        command.CommandText = "SELECT Amount FROM TransactionTable WHERE TransactionID = @TransactionID";
+                        command.Parameters.AddWithValue("@TransactionID", transactionID);
+
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Retrieve the amount from the transaction
+                                return Convert.ToInt32(reader["Amount"]);
+                            }
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving transaction amount: " + ex.Message);
+            }
+
+            return 0; // Return 0 if the transaction doesn't exist or if there's an error
+        }
+        private static void UpdateAccountBalance(string accountNo, string transactionType, int amount)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SQLiteCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"
+                        UPDATE AccountTable
+                        SET Balance = Balance + @Amount
+                        WHERE AccountNo = @AccountNo";
+
+                        if (transactionType == "WITHDRAW")
+                        {
+                            // For withdrawals, subtract the amount from the balance
+                            command.CommandText = @"
+                            UPDATE AccountTable
+                            SET Balance = Balance - @Amount
+                            WHERE AccountNo = @AccountNo";
+                        }
+
+                        command.Parameters.AddWithValue("@AccountNo", accountNo);
+                        command.Parameters.AddWithValue("@Amount", amount);
+
+                        command.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error updating account balance: " + ex.Message);
             }
         }
 
@@ -525,6 +685,48 @@ namespace DC_Assignment_2_NEW.Data
             }
 
             return transaction;
+        }
+
+        public static List<Transaction> GetTransactionsByNo(string accountNo)
+        {
+            List<Transaction> transactionList = new List<Transaction>();
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SELECT * FROM TransactionTable WHERE AccountNo = @AccountNo";
+                        command.Parameters.AddWithValue("@AccountNo", accountNo);
+
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Transaction transaction = new Transaction
+                                {
+                                    AccountNo = reader["AccountNo"].ToString(),
+                                    TransactionID = reader["TransactionID"].ToString(),
+                                    TransactionType = reader["TransactionType"].ToString(),
+                                    Amount = Convert.ToInt32(reader["Amount"])
+                                };
+
+                                transactionList.Add(transaction);
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving transactions: " + ex.Message);
+            }
+
+            return transactionList;
         }
 
         public static bool InsertUserProfile(UserProfile userProfile)
@@ -749,9 +951,9 @@ namespace DC_Assignment_2_NEW.Data
             {
                 int balance = 0;
                 string accountNumber = "", username = "", email = "";
-       
+
                 Account account;
-                for(int i=0; i < initialAccountNo; i++)
+                for (int i = 0; i < initialAccountNo; i++)
                 {
                     account = new Account();
                     generator.GetNextAccount(out accountNumber, out username, out email, out balance);
@@ -759,7 +961,7 @@ namespace DC_Assignment_2_NEW.Data
                     account.Username = username;
                     account.Email = email;
                     account.Balance = balance;
-                    
+
                     //Console.WriteLine(account.AccountToString());
                     Insert(account);
                 }
@@ -846,7 +1048,7 @@ namespace DC_Assignment_2_NEW.Data
             if (CreateTransactionTable())
             {
                 Transaction transaction = new Transaction();
-                
+
                 List<Account> accounts = GetAll();
                 foreach (Account account in accounts)
                 {
@@ -892,5 +1094,6 @@ namespace DC_Assignment_2_NEW.Data
             }
 
         }
+
     }
 }
