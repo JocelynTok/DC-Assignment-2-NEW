7fa1699305647cc64586049d505021368638a3d7
Jasmine Chieng <700036618@student.curtin.edu.my>
2023-10-15 13:22:11 +0800

    added logging for admin activities

--- DC-Assignment-2-NEW/Controllers/AdminController.cs
+++ DC-Assignment-2-NEW/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DC_Assignment_2_NEW.Logging;
+using Microsoft.AspNetCore.Mvc;
+using DC_Assignment_2_NEW.Data;
+using DC_Assignment_2_NEW.Models;
 
 namespace DC_Assignment_2_NEW.Controllers
 {
@@ -29,6 +32,52 @@ namespace DC_Assignment_2_NEW.Controllers
         public IActionResult GetAdminView()
         {
             return PartialView("../Admin/AdminManagementView");
+        }
+
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("admincreateUser")]
+        public IActionResult CreateUser(UserProfile newUser)
+        {
+            // Perform create user logic
+
+            // Log the admin activity
+            string adminUsername= DBManager.GetCurrentAdminByRole();
+            AuditLogger.LogActivity(adminUsername, $"Created user {newUser.Username}");
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("adminupdateUser")]
+        public IActionResult UpdateUser(UserProfile updatedUser)
+        {
+            // Perform update user logic
+
+            // Log the admin activity
+            string adminUsername = DBManager.GetCurrentAdminByRole();
+            AuditLogger.LogActivity(adminUsername, $"Updated user {updatedUser.Username}");
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("admindeleteUser")]
+        public IActionResult DeleteUser(int userId)
+        {
+            // Perform delete user logic
+
+            // Log the admin activity
+            string adminUsername = DBManager.GetCurrentAdminByRole();
+            AuditLogger.LogActivity(adminUsername, $"Deleted user {userId}");
+
+            return Ok();
         }
     }
 }
---
+++ DC-Assignment-2-NEW/Logging/AuditLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DC_Assignment_2_NEW.Logging
+{
+    public static class AuditLogger
+    {
+        private static string logFilePath = "audit_log.txt";
+
+        public static void LogActivity(string adminId, string activity)
+        {
+            string logEntry = $"{DateTime.Now}: Admin {adminId} performed activity: {activity}";
+
+            try
+            {
+                using (StreamWriter writer = File.AppendText(logFilePath))
+                {
+                    writer.WriteLine(logEntry);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur while writing to the log file
+                Console.WriteLine("Error writing to audit log: " + ex.Message);
+            }
+        }
+    }
+}
